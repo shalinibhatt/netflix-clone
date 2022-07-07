fec3417dca5a21eec98f7a6605cc5b688056f9ef
Shalini <shalinibhatt6263@gmail.com>
2022-07-07 15:33:36 +0530

    comriri

--- src/Row.js
+++ src/Row.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
-import YouTube from "react-youtube";
-import movieTrailer from "movie-trailer";
+
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
